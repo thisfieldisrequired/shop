d0088b48089d2cf6a56f24b1f898b55e72b2f301
sudodebug <sudodebug@gmail.com>
2023-06-28 10:22:35 +0500

    Change admin-panel

--- payment/urls.py
+++ payment/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+from . import webhooks
 
 app_name = 'payment'
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('process/', views.payment_process, name='process'),
     path('completed/', views.payment_completed, name='completed'),
     path('canceled/', views.payment_canceled, name='canceled'),
+    path('webhooks/', webhooks.stripe_webhook, name='stripe-webhook'),
 ]
