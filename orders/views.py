255ff5f0bbb37e01657fa4f9054b52022591bcba
sudodebug <sudodebug@gmail.com>
2023-06-28 11:01:02 +0500

    Added pdf create

--- orders/views.py
+++ orders/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.admin.views.decorators import staff_member_required
 from django.urls import reverse
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import render_to_string
 from .models import OrderItem, Order
 from .forms import OrderCreateForm
 from cart.cart import Cart
 from .tasks import order_created
+import weasyprint
+
+
+@staff_member_required
+def admin_order_pdf(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    html = render_to_string('orders/order/pdf.html', {'order': order})
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'filename=order_{order_id}.pdf'
+    weasyprint.HTML(string=html).write_pdf(response, stylesheets=[weasyprint.CSS(settings.STATIC_ROOT / 'css/pdf.css')])
+    return response
 
 
 def order_create(request):
