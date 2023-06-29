a414c365c14fe549232cf6a1ad43adc6fbf75715
sudodebug <sudodebug@gmail.com>
2023-06-29 09:04:49 +0500

    Added pdf asynx

---
+++ payment/tasks.py
@@ -0,0 +1,21 @@
+from io import BytesIO
+from celery import shared_task
+import weasyprint
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
+from django.conf import settings
+from orders.models import Order
+
+
+@shared_task
+def payment_completed(order_id):
+    order = Order.objects.get(id=order_id)
+    subject = f'Shop - Invoice no. {order.id}'
+    message = f'Info at attache'
+    email = EmailMessage(subject, message, 'admin@admin.admin', [order.email])
+    html = render_to_string('orders/order/pdf.html', {'order': order})
+    out = BytesIO()
+    stylesheets = [weasyprint.CSS(settings.STATIC_ROOT / 'css/pdf.css')]
+    weasyprint.HTML(string=html).write_pdf(out, stylesheets=stylesheets)
+    email.attach(f'order_{order.id}.pdf', out.getvalue(), 'application/pdf')
+    email.send()
