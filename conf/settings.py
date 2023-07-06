8db2abc19932d27383ce2113d529e4b4fec1d194
sudodebug <sudodebug@gmail.com>
2023-07-06 10:09:26 +0500

    Added rosetta translation

--- conf/settings.py
+++ conf/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rosetta',
+
     'shop.apps.ShopConfig',
     'cart.apps.CartConfig',
     'orders.apps.OrdersConfig',
